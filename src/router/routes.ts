b21075e0839d88261a6f7f5145d69f2734de21da
lynx-r <lynx.p9@gmail.com>
2021-02-07 22:07:34 +0300

    clean

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,72 +4,70 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     redirect: 'home',
-    component: () => import('~/pages/Home.vue'),
+    component: () => import('~/views/Home.vue'),
     children: [
       {
         path: 'home',
         name: 'home',
-        component: () => import('~/pages/HomeGlobal.vue')
+        component: () => import('~/views/HomeGlobal.vue')
       },
       {
         path: 'my-feed',
         name: 'home-my-feed',
-        component: () => import('~/pages/HomeMyFeed.vue')
+        component: () => import('~/views/HomeMyFeed.vue')
       },
       {
         path: 'tag/:tag',
         name: 'home-tag',
-        component: () => import('~/pages/HomeTag.vue')
-      },
-      {
-        path: '/@:username',
-        component: () => import('~/pages/Profile.vue'),
-        children: [
-          {
-            path: 'profile',
-            name: 'profile',
-            component: () => import('~/pages/ProfileArticles.vue')
-          },
-          {
-            name: 'profile-favorites',
-            path: 'favorites',
-            component: () => import('~/pages/ProfileFavorited.vue')
-          }
-        ]
-      },
-      {
-        name: 'article',
-        path: 'article',
-        component: () => import('~/pages/HomeGlobal.vue')
+        component: () => import('~/views/HomeTag.vue')
       }
     ],
   },
   {
     name: 'login',
     path: '/login',
-    component: () => import('~/pages/Login.vue')
+    component: () => import('~/views/Login.vue')
   },
   {
     name: 'register',
     path: '/register',
-    component: () => import('~/pages/Register.vue')
+    component: () => import('~/views/Register.vue')
   },
   {
     name: 'settings',
     path: '/settings',
-    component: () => import('~/pages/Settings.vue')
+    component: () => import('~/views/Settings.vue')
+  },
+  // Handle child routes with a default, by giving the name to the
+  // child.
+  // SO: https://github.com/vuejs/vue-router/issues/777
+  {
+    path: '/@:username',
+    component: () => import('~/views/Profile.vue'),
+    children: [
+      {
+        path: 'profile',
+        name: 'profile',
+        component: () => import('~/views/ProfileArticles.vue')
+      },
+      {
+        name: 'profile-favorites',
+        path: 'favorites',
+        component: () => import('~/views/ProfileFavorited.vue')
+      }
+    ]
   },
   {
     name: 'article',
     path: '/articles/:slug',
-    component: () => import('~/pages/Article.vue'),
+    component: () => import('~/views/Article.vue'),
     props: true
   },
   {
     name: 'article-edit',
     path: '/editor/:slug?',
     props: true,
-    component: () => import('~/pages/ArticleEdit.vue')
+    component: () => import('~/views/ArticleEdit.vue')
   }
 ]
 export default routes
